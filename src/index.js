79e57ef2f8f56727209b84201196116a458f8468
der1x <130235333+Mark4353@users.noreply.github.com>
2026-01-31 17:59:46 +0200

    Fix App.css import case sensitivity

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import App from "./App";
-import "./App.css";
+import "./app.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <BrowserRouter basename="/search01/movie-search/">
